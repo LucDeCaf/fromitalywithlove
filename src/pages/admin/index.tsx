0dc8f3003df6be2b4e7a3647a58461e02c2eeab7
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2022-09-18 15:38:12 +0200

    Added landing admin page w/ image accordion

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,43 +1,57 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Accordion } from "react-bootstrap";
 import { GetStaticProps } from "next";
+import { nanoid } from "nanoid";
+import type { ImageType } from "../../utils/types";
+import Link from "next/link";
 
-const Page = ({ images }) => {
+const Page = ({ images }: { images: ImageType[] }) => {
   return (
     <main>
       <Container>
-        <Row>
-          <Col xs={6}>
-            <h1>Welcome, user</h1>
-            <h3>Manage your content</h3>
+        <div className="text-info">
+          <h1 className="text-center text-decoration-underline display-4">
+            Welcome, MG
+          </h1>
+          <h3 className="text-center mb-4 display-6">Manage your content</h3>
+        </div>
+        <h3 className="text-center text-decoration-underline h1 text-primary">
+          Images
+        </h3>
+        <Accordion>
+          {images.map((image, index) => {
+            const editLink = `/admin/manage/image?id=${image.id}`;
 
-          </Col>
-          <div>
-            
-          </div>
-        </Row>
+            return (
+              <Accordion.Item key={nanoid()} eventKey={index.toString()}>
+                <Accordion.Header>{image.label}</Accordion.Header>
+                <Accordion.Body>
+                  Description: &quot;{image.desc}&quot;
+                  <br />
+                  <Link href={editLink}><a>Edit image {image.id}</a></Link>
+                </Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
+        </Accordion>
       </Container>
     </main>
   );
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  let images = [];
-  try {
-    const res = await fetch(
-      "https://fromitalywithlove.vercel.app/api/get/images"
-    );
-    const data = await res.json();
-    if (data.success === false) throw Error(data.message);
+  const res = await fetch(
+    "https://fromitalywithlove.vercel.app/api/get/images"
+  );
+  const data = await res.json();
+  if (data.success === false) throw Error(data.message);
 
-    images = await data.data;
-  } catch (err) {
-    console.error(err.message);
-  }
+  const images = await data.data;
 
   return {
     props: {
       images: images,
     },
+    revalidate: 10,
   };
 };
 
